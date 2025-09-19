fba6f14c24811dba08dc31c06baedbc910515bf5
Casper <casper141@gmail.com>
2025-09-19 14:04:32 +0300

    week 3

---
+++ src/test/java/app/TemperatureConverterTest.java
@@ -0,0 +1,26 @@
+package app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fahrenheitToCelsius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void celsiusToFahrenheit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isExtremeTemperature() {
+    }
+}
